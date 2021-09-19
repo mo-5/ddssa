fbadda12c3047c42ba78a3f2af01509d59ef8648
John Breton <55368611+john-breton@users.noreply.github.com>
2021-09-19 16:37:50 -0400

    Initial Python parsing
    - Does not calculate CCP, currently just looks at a Python file and produces data regarding the number of methods, and the parameters associated with each of those methods.
    - Next step is to use this data to determine the CCP per method, and then from that report the method with the highest CCP in the file. Ideas that come to mind immediately is to immediately disregard methods with no parameters as those cannot be the "parent" methods (unless this is the __main__ method, then an exception would need to be made.

---
+++ CCPPrototype/ccp_parser_py.py
@@ -0,0 +1,53 @@
+import os
+
+
+class CCPParserPy:
+    """ The goal of this class is to parse data from a target Python
+    file and then use said data to calculate the maximum CCP of the
+    class while providing the name of the method that produced that
+    max CCP value
+    """
+
+    def collect_method_names(self, target_file):
+        names = []
+        with open(target_file) as f:
+            for line in f.readlines():
+                if "def" in line and line.strip()[0:4] == "def ":
+                    names.append(line.split("(")[0].split(" ")[-1])
+        return names
+
+    def parameters_per_method(self, method_name, target_file):
+        with open(target_file) as f:
+            for line in f.readlines():
+                if "def" in line and method_name in line:
+                    return line.split("(")[-1].strip(
+                        "self").strip(",").split(")")[0].strip(" ")
+
+
+def parse_target_file(path):
+    method_names = ccp_tester.collect_method_names(path)
+    print("Number of methods: " + str(len(method_names)))
+    print("Method names:")
+    for name in method_names:
+        print("\t" + name)
+    for i in range(len(method_names)):
+        print("Parameter(s) in method {}".format(method_names[i]))
+        parameters = ccp_tester.parameters_per_method(
+            method_names[i], path)
+        if len(parameters) == 0:
+            print("\tNo parameters (0)")
+        else:
+            print("\t{} ({})".format(parameters, len(parameters.split(","))))
+
+
+if __name__ == '__main__':
+    ccp_tester = CCPParserPy()
+    file_path = os.path.join(os.getcwd(), "SamplePythonFiles", "target_py.py")
+    file_path_2 = os.path.join(
+        os.getcwd(), "SamplePythonFiles", "test_zoia_py.py")
+
+    print("Test 1: target_py.py")
+    parse_target_file(file_path)
+
+    print("\nTest 2: test_zoia_py.py")
+    parse_target_file(file_path_2)

9d1af9aaf83836d877258f9b08b8742ee9672a49
John Breton <55368611+john-breton@users.noreply.github.com>
2021-11-02 12:21:52 -0400

    Expand AST Parsing
    - Introduced a dummy ASTCoordinator class that will act as the controller being the interface and backend model (ASTSuppliers).
      - The goal of ASTCoordinator will be to spawn threads that will be bound to a single ASTSupplier. These bound suppliers will parse a subset of the files that have been requested for analysis to increase static analysis performance. For now, that multi-threading functionality is absent.
    - Added additional logic to ASTSupplier to handle parsing out loops from a supplied file.

--- capstone_project/backend/ast_supplier.py
+++ capstone_project/backend/ast_supplier.py
@@ -3,16 +3,58 @@ import ast
 
 class ASTSupplier:
     def __init__(self):
-        self._content = []
+        self._node = None
+        self._loops = ast.For, ast.While, ast.AsyncFor
 
-    def create_ast_from_file(self, files):
-        for file in files:
-            with open(file, "rb") as f:
-                self._content.append(ast.parse(f.read()))
-
-    def print_parsed_ast(self):
-        for content in self._content:
-            print(ast.unparse(content))
+    def create_ast_from_file(self, file):
+        with open(file, "rb") as f:
+            self._node = ast.parse(f.read())
 
     def get_ast_list(self):
-        return self._content
+        return self._node
+
+    def get_loop_nodes_for_file(self):
+        """ Return the loop nodes from the created AST that
+        was generated for a specific file. Loop nodes are
+        defined as while, for, and async for loops.
+        """
+        # TODO We should update this to only include top level loops,
+        #      since if a loop contains another loop, we will already
+        #      have it from the top-level node being added.
+        nodes = ast.walk(self._node)
+        loop_nodes = []
+        for node in nodes:
+            if isinstance(node, self._loops):
+                loop_nodes.append(node)
+        return loop_nodes
+
+    def has_loop_nodes(self):
+        """ Returns True if for, while, or async for loops are detected.
+        """
+        nodes = ast.walk(self._node)
+        return any(isinstance(node, self._loops) for node in nodes)
+
+    def _print_parsed_ast(self):
+        """ Logging method to print the generated AST.
+        """
+        print(self._node.__dict__)
+
+    def _print_unparsed_ast(self):
+        """ Logging method to print the unparsed version of
+        the generated AST.
+        """
+        print(ast.unparse(self._node))
+
+
+if __name__ == '__main__':
+    # Functionality demonstration. This class will not contain a
+    # main method.
+    ast_test = ASTSupplier()
+    ast_test.create_ast_from_file("directory_parser.py")
+    ast_test._print_unparsed_ast()
+    ast_test._print_parsed_ast()
+    print(ast_test.has_loop_nodes())
+    file_loops = ast_test.get_loop_nodes_for_file()
+    print(file_loops)
+    for loop in file_loops:
+        print(ast.unparse(loop))

9c3825e3792bef4a54b0604b05896ac9b1b47905
John Breton <55368611+john-breton@users.noreply.github.com>
2021-11-21 20:09:54 -0500

    Update SRCalculator to use a JSON reference
    - Replace the .txt reference file with a organized JSON file in case additional references are needed for future static analyzers

--- capstone_project/backend/sr_calculator.py
+++ capstone_project/backend/sr_calculator.py
@@ -1,5 +1,6 @@
 import ast
 import os
+import json
 
 
 class SRCalculator:
@@ -8,16 +9,16 @@ class SRCalculator:
     """
     def __init__(self, filename):
         self._filename = filename
-        # TODO Use JSON instead for this.
-        self._reference_file = "stall_statements.txt"
+        self._reference_file = "stall_statements.json"
+        self._sr_access = "sr_statements"
 
     def set_filename(self, filename):
         self._filename = filename
 
     def calculate_sr(self, nodes):
-        reference = []
-        with open(os.path.join(os.path.dirname(__file__), self._reference_file), "r") as f:
-            reference = f.readline().split(",")
+        with open(os.path.join(os.path.dirname(__file__),
+                               self._reference_file), "r") as f:
+            reference = json.load(f)
 
         score = 0
         for node in nodes:
@@ -25,13 +26,7 @@ class SRCalculator:
             for line in lines:
                 # TODO discuss if multiple statements per line would
                 #   be grounds to increment SR multiple times.
-                if any(stall in line for stall in reference):
+                if any(stall in line for stall in reference[self._sr_access]):
                     score += 1
         print("File {} has a calculated stall ratio of {}".format(
             self._filename, score))
-
-
-
-if __name__ == '__main__':
-    sr = SRCalculator("Test")
-    sr.calculate_sr(None)

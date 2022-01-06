d3d4f37986580fc8db3797b8bbb7fd5d97b87e83
John Breton <55368611+john-breton@users.noreply.github.com>
2022-01-06 16:32:16 -0500

    Add unit tests for the requirements.txt parser.

---
+++ capstone_project/tests/test_package_parsers.py
@@ -0,0 +1,116 @@
+""" tests contains any unit tests for backend files
+"""
+import os
+import unittest
+
+from capstone_project.backend.package_ids import PackageIds
+from capstone_project.backend.package_parser import PackageParser
+from capstone_project.backend.req_parser import RequirementsParser
+
+
+def create_parser(file):
+    """Generate a generic parser on request"""
+    return PackageParser(file)
+
+
+class TestPackageParsers(unittest.TestCase):
+    """TestPackageParsers is responsible for testing the various
+    package parsers that are used to determine the packages associated
+    with a Python project."""
+
+    def test_req_parser(self):
+        """Test a typical requirements.txt file to ensure all packages,
+        versions, and search ranges are correctly identified.
+        """
+        req_parser = create_parser(
+            os.path.join(
+                os.getcwd(),
+                "capstone_project",
+                "tests",
+                "package_test_files",
+                "requirements.txt",
+            )
+        )
+        req_parser.__class__ = RequirementsParser
+
+        req_parser.begin_analysis()
+
+        # Check to see all requirements were identified.
+        size = req_parser.get_data().shape[1]
+        self.assertTrue(
+            size == 12,
+            f"Number of requirements identified was not correct. Expected 12 "
+            f"but got {size}",
+        )
+
+        # Check to see if the first search range was correctly identified.
+        search_range = req_parser.get_data()["0"].to_numpy()[2]
+        self.assertTrue(
+            search_range == PackageIds.MAX,
+            f"The search range for the requirement was not correct. Expected "
+            f"MAX but got {search_range}",
+        )
+
+        # Check to see if the first name was correctly identified.
+        name = req_parser.get_data()["0"].to_numpy()[0]
+        self.assertTrue(
+            name == "psutil",
+            f"The name for the requirement was not correct. Expected psutil "
+            f"but got {name}",
+        )
+
+        # Check to see if the first version was correctly identified.
+        version = req_parser.get_data()["0"].to_numpy()[1]
+        self.assertTrue(
+            version[0] == (">=", "5.7.0"),
+            f"The version for the requirement was not correct. "
+            f'Expected (">=", "5.7.0") but got {version}.',
+        )
+
+        # Check to see if each range is identified correctly.
+        search_range = req_parser.get_data()["1"].to_numpy()[2]
+        self.assertTrue(
+            search_range == PackageIds.SINGLE,
+            f"The search range for the requirement was not correct. Expected "
+            f"SINGLE but got {search_range}",
+        )
+
+        search_range = req_parser.get_data()["10"].to_numpy()[2]
+        self.assertTrue(
+            search_range == PackageIds.RANGE,
+            f"The search range for the requirement was not correct. Expected "
+            f"RANGE but got {search_range}",
+        )
+
+        # Check to see that the size of the versions array in a requirement
+        # with a RANGE is 2.
+        version = req_parser.get_data()["0"].to_numpy()[1]
+        self.assertTrue(
+            len(version[0]) == 2,
+            f"The number of versions for the requirement was not correct. "
+            f"Expected 2 but got {len(version[0])}.",
+        )
+
+    def test_req_parser_error(self):
+        """Test a malformed requirements.txt file to an error is thrown
+        such that is can be caught during actual analysis.
+        """
+        req_parser = create_parser(
+            os.path.join(
+                os.getcwd(),
+                "capstone_project",
+                "tests",
+                "package_test_files",
+                "bad_requirements.txt",
+            )
+        )
+        req_parser.__class__ = RequirementsParser
+
+        try:
+            req_parser.begin_analysis()
+            self.fail(
+                "Analysis should not complete successfully for an "
+                "incorrectly formatted requirements.txt file"
+            )
+        except Exception:
+            pass

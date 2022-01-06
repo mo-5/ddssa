d57bdf2c8602576af083a2774f292e3f5e8c4e7e
John Breton <55368611+john-breton@users.noreply.github.com>
2022-01-06 15:40:32 -0500

    Use requirements-parser
    Remove home-baked parser solution and replace it with requirements-parser (comes equipped with error handling).

--- capstone_project/backend/req_parser.py
+++ capstone_project/backend/req_parser.py
@@ -1,4 +1,4 @@
-import os
+import requirements
 
 from capstone_project.backend.package_ids import PackageIds
 from capstone_project.backend.package_parser import PackageParser
@@ -14,15 +14,18 @@ class RequirementsParser(PackageParser):
     def parse_packages(self):
         """Parse out package information from a
         requirements.txt file"""
-        with open(os.path.join(os.path.dirname(__file__), self._filename), "r") as file:
-            for i, line in enumerate(file.readlines()):
-                name = line.split(">")[0].split("~")[0].split("=")[0]
-                version = [line.split("=")[-1].strip("=").strip()]
-                if ">" in line:
+        with open(self._filename, "r") as f:
+            for i, req in enumerate(requirements.parse(f)):
+                if len(req.specs) > 1:
+                    search_range = PackageIds.RANGE
+                elif ">" in req.specs[0][0] and len(req.specs) == 1:
                     search_range = PackageIds.MAX
                 else:
                     search_range = PackageIds.SINGLE
-                self._package_data[str(i)] = [name, version, search_range]
+
+                self._package_data[str(i)] = [req.name, req.specs, search_range]
+
+        print(self._package_data.to_string())
 
     def get_data(self):
         """Return the collected package information"""

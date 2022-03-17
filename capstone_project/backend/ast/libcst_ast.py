b09cf38b1b6c950fb08f994b3aa4adfb4a73a76a
KhalilAalab <khalilaalab@cmail.carleton.ca>
2022-03-17 18:59:12 -0400

    Fixing formatting using black

--- capstone_project/backend/ast/libcst_ast.py
+++ capstone_project/backend/ast/libcst_ast.py
@@ -2,23 +2,24 @@ import libcst
 import libcst.metadata
 import json
 
+
 def main():
-    
+
     file_to_parse = "capstone_project/tests/static_test_files/sr_medium.py"
 
     with open(
-            file_to_parse,
-            "r",
-            encoding="UTF-8",
-        ) as file:
-            file_contents = file.read()
+        file_to_parse,
+        "r",
+        encoding="UTF-8",
+    ) as file:
+        file_contents = file.read()
 
     with open(
-            "capstone_project/backend/metrics/stall_statements.json",
-            "r",
-            encoding="UTF-8",
-        ) as file:
-            reference = json.load(file)
+        "capstone_project/backend/metrics/stall_statements.json",
+        "r",
+        encoding="UTF-8",
+    ) as file:
+        reference = json.load(file)
 
     module = libcst.parse_module(file_contents)
 
@@ -28,21 +29,29 @@ def main():
         def __init__(self):
             self.score = 0
             self.stall_dict = {}
-            
+
         def visit_For_body(self, node) -> None:
-            pos = self.get_metadata(libcst.metadata.WhitespaceInclusivePositionProvider, node).start
+            pos = self.get_metadata(
+                libcst.metadata.WhitespaceInclusivePositionProvider, node
+            ).start
             lines = module.code_for_node(node).splitlines()
             for index, line in enumerate(lines):
                 if any(stall in line for stall in reference["sr_statements"]):
                     if (pos.line + index) not in self.stall_dict:
                         self.score += 1
-                        pos = self.get_metadata(libcst.metadata.WhitespaceInclusivePositionProvider, node).start
+                        pos = self.get_metadata(
+                            libcst.metadata.WhitespaceInclusivePositionProvider, node
+                        ).start
                         actual_line = int(str(pos.line)) + index
-                        print(f"stall statement '{line} found at actual line {actual_line}")
+                        print(
+                            f"stall statement '{line} found at actual line {actual_line}"
+                        )
                         self.stall_dict[actual_line] = line
 
         def visit_While_body(self, node) -> None:
-            pos = self.get_metadata(libcst.metadata.WhitespaceInclusivePositionProvider, node).start
+            pos = self.get_metadata(
+                libcst.metadata.WhitespaceInclusivePositionProvider, node
+            ).start
             lines = module.code_for_node(node).splitlines()
             for index, line in enumerate(lines):
                 if any(stall in line for stall in reference["sr_statements"]):
@@ -54,11 +63,12 @@ def main():
 
         def get_score(self):
             return self.score
- 
+
     stall_visitor = StallVisitor()
     wrapper_actual_file = libcst.metadata.MetadataWrapper(module)
     wrapper_actual_file.visit(stall_visitor)
-    print("Total number of stall statements: " + str(stall_visitor.get_score())) 
+    print("Total number of stall statements: " + str(stall_visitor.get_score()))
+
 
 if __name__ == "__main__":
     main()
--- capstone_project/tests/static_test_files/sr_medium.py
+++ capstone_project/tests/static_test_files/sr_medium.py
@@ -14,33 +14,31 @@ print("before loop")
 for i in range(8):
     if i == 7:
         warnings.warn("The loop is about to end!")  # Stall
-# this is a random comment
+    # this is a random comment
 
     print("Hello, World!")  # Stall
     time.sleep(0.01)  # Stall
 
-# # this is a random comment
+    # # this is a random comment
 
     # # this is a random comment
 
     """Random 
     multi-line comment"""
-    
 
     pprint("this is an object")
-    
 
     os.system('echo "hello"')
-    os.system('pause')
+    os.system("pause")
     logging.basicConfig(level=logging.INFO)
 
     logging.info("here is some info")
-    
+
 print("after loop")
 
 k = 1
 
-while(k < 5):
+while k < 5:
     pprint("inside while loop!")
 
 print("outside loop")

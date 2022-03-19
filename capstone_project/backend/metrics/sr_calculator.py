5b200a36f23dea60062e154bd7bb160ed8e895ce
KhalilAalab <khalilaalab@cmail.carleton.ca>
2022-03-19 18:45:20 -0400

    Reintegrating frivolous operations into code

--- capstone_project/backend/metrics/sr_calculator.py
+++ capstone_project/backend/metrics/sr_calculator.py
@@ -37,6 +37,7 @@ class SRCalculator:
         ) as file:
             reference = json.load(file)
 
+        # Handle simple cases with pattern matching
         stall_visitor = StallVisitor(libcst_module, reference)
         wrapper_actual_file = libcst.metadata.MetadataWrapper(libcst_module)
         wrapper_actual_file.visit(stall_visitor)
@@ -45,72 +46,66 @@ class SRCalculator:
         for key, value in stall_dict.items():
             self._sr_detections[-1].append((key, value))
 
-        # score_average = []
-        # stall_total = 0
-        # stall_ratio = 0
-        # for node in nodes:
-        # score = 0
-        # Handle simple cases with pattern matching
-        # lines = ast.unparse(node).splitlines()
-        # for index, line in enumerate(lines):
-        #     if any(stall in line for stall in reference[self._sr_access]):
-        #         score += 1
-        #         self._sr_detections[-1].append((node.lineno + index, line))
+        score_average = []
+        stall_total = 0
+        stall_ratio = 0
+        for node in nodes:
+            score = 0
+            
+            # Handle more complex cases for frivolous operations
+            for sub_node in ast.walk(node):
+                if isinstance(sub_node, ast.BinOp):
+                    # Handle the case where 0 is being added or subtracted
+                    if isinstance(sub_node.op, (ast.Add, ast.Sub)) and (
+                        (
+                            isinstance(sub_node.left, ast.Constant)
+                            and sub_node.left.value == 0
+                        )
+                        or (
+                            isinstance(sub_node.right, ast.Constant)
+                            and sub_node.right.value == 0
+                        )
+                    ):
+                        score = self._complex_update_score(sub_node, score)
+                    # Handle the case where 1 is being multiplied or divided
+                    elif isinstance(
+                        sub_node.op, (ast.Mult, ast.Div, ast.FloorDiv)
+                    ) and (
+                        (
+                            isinstance(sub_node.left, ast.Constant)
+                            and sub_node.left.value == 1
+                        )
+                        or (
+                            isinstance(sub_node.right, ast.Constant)
+                            and sub_node.right.value == 1
+                        )
+                    ):
+                        score = self._complex_update_score(sub_node, score)
+                elif isinstance(sub_node, ast.AugAssign):
+                    # Handle the case where 0 is being added or subtracted
+                    if (
+                        isinstance(sub_node.op, (ast.Add, ast.Sub))
+                        and isinstance(sub_node.value, ast.Constant)
+                        and sub_node.value.value == 0
+                    ):
+                        score = self._complex_update_score(sub_node, score)
+                    # Handle the case where 1 is being multiplied or divided
+                    elif (
+                        isinstance(sub_node.op, (ast.Mult, ast.Div, ast.FloorDiv))
+                        and isinstance(sub_node.value, ast.Constant)
+                        and sub_node.value.value == 1
+                    ):
+                        score = self._complex_update_score(sub_node, score)
 
-        # # Handle more complex cases for frivolous operations
-        # for sub_node in ast.walk(node):
-        #     if isinstance(sub_node, ast.BinOp):
-        #         # Handle the case where 0 is being added or subtracted
-        #         if isinstance(sub_node.op, (ast.Add, ast.Sub)) and (
-        #             (
-        #                 isinstance(sub_node.left, ast.Constant)
-        #                 and sub_node.left.value == 0
-        #             )
-        #             or (
-        #                 isinstance(sub_node.right, ast.Constant)
-        #                 and sub_node.right.value == 0
-        #             )
-        #         ):
-        #             score = self._complex_update_score(sub_node, score)
-        #         # Handle the case where 1 is being multiplied or divided
-        #         elif isinstance(
-        #             sub_node.op, (ast.Mult, ast.Div, ast.FloorDiv)
-        #         ) and (
-        #             (
-        #                 isinstance(sub_node.left, ast.Constant)
-        #                 and sub_node.left.value == 1
-        #             )
-        #             or (
-        #                 isinstance(sub_node.right, ast.Constant)
-        #                 and sub_node.right.value == 1
-        #             )
-        #         ):
-        #             score = self._complex_update_score(sub_node, score)
-        #     elif isinstance(sub_node, ast.AugAssign):
-        #         # Handle the case where 0 is being added or subtracted
-        #         if (
-        #             isinstance(sub_node.op, (ast.Add, ast.Sub))
-        #             and isinstance(sub_node.value, ast.Constant)
-        #             and sub_node.value.value == 0
-        #         ):
-        #             score = self._complex_update_score(sub_node, score)
-        #         # Handle the case where 1 is being multiplied or divided
-        #         elif (
-        #             isinstance(sub_node.op, (ast.Mult, ast.Div, ast.FloorDiv))
-        #             and isinstance(sub_node.value, ast.Constant)
-        #             and sub_node.value.value == 1
-        #         ):
-        #             score = self._complex_update_score(sub_node, score)
+            stall_total += score
+            score_average.append(score / len(ast.unparse(node).split("\n")))
 
-        #     stall_total += score
-        #     score_average.append(score / len(ast.unparse(node).split("\n")))
-
-        # for score in score_average:
-        #     stall_ratio += score
-        # if len(score_average) > 0:
-        #     stall_ratio = stall_ratio / len(score_average)
-        # else:
-        #     stall_ratio = 0
+        for score in score_average:
+            stall_ratio += score
+        if len(score_average) > 0:
+            stall_ratio = stall_ratio / len(score_average)
+        else:
+            stall_ratio = 0
 
         return self._sr_detections
 

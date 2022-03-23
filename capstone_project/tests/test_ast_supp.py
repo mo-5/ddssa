fc8906d29fcc336f490baaea7c25d54f3ab6952f
KhalilAalab <khalilaalab@cmail.carleton.ca>
2022-03-23 10:26:26 -0400

    Adding test cases for testing ast/cst supplier

--- capstone_project/tests/test_ast_supp.py
+++ capstone_project/tests/test_ast_supp.py
@@ -1,6 +1,12 @@
 """ tests contains any unit tests for backend files
 """
 import unittest
+import os
+from capstone_project.backend.cst.cst_supplier import CSTSupplier
+from capstone_project.tests.test_utils import TestUtils
+import io
+import unittest.mock
+from contextlib import redirect_stdout
 
 
 class TestASTSupplier(unittest.TestCase):
@@ -9,5 +15,45 @@ class TestASTSupplier(unittest.TestCase):
     Python files.
     """
 
-    def test_something(self):
-        """To be written."""
+    def setUp(self):
+        # Clear object instance for each test
+        self._test_path = TestUtils().get_test_path()
+        self._cst_supplier = CSTSupplier()
+        self._cst_supplier.create_ast_from_file(
+            os.path.join(
+                self._test_path,
+                "static_test_files",
+                "sr_simple.py",
+            )
+        )
+
+        self._cst_supplier.get_libcst_module_from_file(
+            os.path.join(
+                self._test_path,
+                "static_test_files",
+                "sr_simple.py",
+            )
+        )
+
+    def test_has_loop_nodes(self):
+        """Test to see if the supplier can find loop nodes in a file"""
+        self.assertTrue(
+            self._cst_supplier.has_loop_nodes() == True,
+            "Should have detected loops nodes in this file",
+        )
+        self.assertTrue(
+            len(self._cst_supplier.get_loop_nodes_for_file()) == 1,
+            "Should have detected 1 loop in this file",
+        )
+
+    def test_unparsed_ast(self):
+        with redirect_stdout(io.StringIO()) as f:
+            self._cst_supplier._print_parsed_ast()
+        s = f.getvalue()
+        self.assertTrue("{'body': " in s)
+
+    def test_unparsed_ast(self):
+        with redirect_stdout(io.StringIO()) as f:
+            self._cst_supplier._print_unparsed_ast()
+        s = f.getvalue()
+        self.assertTrue("import time" in s)
